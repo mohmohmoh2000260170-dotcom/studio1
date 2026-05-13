bea773806b0cf8229a6a98ffcc667a6cecda33a6
MOHAMMAD AL KHALAILEH <mohmohmoh2000260170@gmail.com>
2026-05-13 13:04:43 +0000

    تأكدي من التطبيق أنه يعمل وتأكد من من كل الملفات عشان اوديهم على github

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* Production Optimization Config */
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -13,24 +12,17 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
       },
     ],
   },
-  // Optimizing for mobile web views
   compress: true,
   reactStrictMode: true,
   poweredByHeader: false,
